f2021a692d87d877ac75f03e6e0f22c0ea3b75dc
Dheeraj Sharma <dextersharma960@gmail.com>
2022-03-07 19:09:04 +0530

    Migrated to v2

--- android/app/src/main/kotlin/com/example/cab_booking/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/cab_booking/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.cab_booking
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+
+class MainActivity : FlutterActivity() { // You do not need to override onCreate() in order to invoke
+    // GeneratedPluginRegistrant. Flutter now does that on your behalf.
+    // ...retain whatever custom code you had from before (if any).
 }
